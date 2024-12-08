b88f6bc0b1f45afceccef6f285209c07c46687c2
최재용 <choiyongyong0813@gmail.com>
2024-12-08 14:37:45 +0900

    .

--- Board/src/test/java/com/study/BoardApplicationTests.java
+++ Board/src/test/java/com/study/BoardApplicationTests.java
@@ -1,13 +1,40 @@
 package com.study;
 
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class BoardApplicationTests {
+	@Autowired
+	private ApplicationContext context;
+	@Autowired
+	private SqlSessionFactory sessionFactory;
 
 	@Test
 	void contextLoads() {
+	}
+	@Test
+	public void testContext() {
+		try {
+			System.out.println("=================================================");
+			System.out.println(context.getBean("SqlSessionFactory"));
+			System.out.println("=================================================");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	@Test
+	public void testSqlSessionFactory() {
+		try {
+			System.out.println("=================================================");
+			System.out.println(context.getBean(sessionFactory.toString()));
+			System.out.println("=================================================");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
